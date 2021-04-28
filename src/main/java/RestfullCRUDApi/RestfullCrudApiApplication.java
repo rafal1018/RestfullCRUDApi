5cc6beb1d0c7ce5bae10d3766dc8ebdb4d031383
rafal1018 <rafaljaneczek@o2.pl>
2021-04-28 21:12:45 +0200

    initial commit

---
+++ src/main/java/RestfullCRUDApi/RestfullCrudApiApplication.java
@@ -0,0 +1,13 @@
+package RestfullCRUDApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RestfullCRUDApi/RestfullCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package RestfullCRUDApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
