24d5c6339c9c5be31c631260c96aee608e1b4e84
rafal1018 <rafaljaneczek@o2.pl>
2021-04-28 21:30:22 +0200

    User entity

---
+++ src/main/java/RestfullCRUDApi/entity/User.java
@@ -0,0 +1,32 @@
+package RestfullCRUDApi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+}
