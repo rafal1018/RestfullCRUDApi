379dc7ae3adc278496eb085011715134c5f8d41b
rafal1018 <rafaljaneczek@o2.pl>
2021-04-28 21:35:13 +0200

    Custom Business Exception

---
+++ src/main/java/RestfullCRUDApi/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package RestfullCRUDApi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
